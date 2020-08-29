80f7a69cd9fff1a3ab49a9b23804f9773f3cf96e
goranaviani <goranaviani@gmail.com>
2020-08-29 17:03:42 +0200

    added name box

--- helloworld/src/helloworld/app.py
+++ helloworld/src/helloworld/app.py
@@ -16,12 +16,24 @@ class HelloWorld(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        main_box = toga.Box()
+        main_box = toga.Box(style=Pack(direction=COLUMN))
+
+        name_label = toga.Label(
+            'Your name: ',
+            style=Pack(padding=(0, 5))
+        )
+        self.name_input = toga.TextInput(style=Pack(flex=1))
+
+
+        name_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        name_box.add(name_label)
+        name_box.add(self.name_input)
+
+        main_box.add(name_box)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
-
 
 def main():
     return HelloWorld()
